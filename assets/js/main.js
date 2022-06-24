61a8fbab8e3e0bf7e5163a7526f86b5d5ba8b546
ehtovk <vicaetano_barros@hotmail.com>
2022-06-23 23:55:43 -0300

    Added new functions to support messages and send messages

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,12 +40,18 @@ function selectPrivacy(element) {
 
 let nome;
 let nomes;
+let messages_On_Screen = document.querySelector(".messages ul");
+let user_On_Screen = document.querySelector(".contact");
+let message;
+let my_message;
+let my_message_obj;
+let cadastro_Nome = {};
 
 /*======== FUNÇÃO DE CADASTRO NO NOME DE ENTRADA =============*/
 
 function joinUser() {
     nome = document.querySelector(".join-page>input").value;
-    let cadastro_Nome = 
+    cadastro_Nome = 
     {
         name: nome
     }
@@ -57,14 +63,18 @@ function joinUser() {
     
 }
 
+function verifyUser() {
+    let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', cadastro_Nome);
+}
+
+setInterval(verifyUser, 5000);
+
 /*======== FUNÇÃO DE VERIFICAR A ENTRADA ================*/
 
-function verifyEntrance(answer) {
-    let verify = answer.status;
-    console.log(verify);
+function verifyEntrance() {
     closeJoinPage();
     getUsers();
-    seeUsers();
+    getMessages();
 }
 
 /*======== FUNÇÃO DE VERIFICAR ERRO ================*/
@@ -78,13 +88,38 @@ function verifyError(error) {
 }
 
 function getMessages() {
-    axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
-    .then(response => {
-        //console.log(response.data)
-    })
-    .catch(error => console.log(error))
+    let promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promisse.then(seeMessages);
 }
-getMessages();
+
+function seeMessages(messages) {
+    messages_On_Screen.innerHTML="";
+    message = messages.data;
+    message.forEach(addMessages);
+}
+
+function addMessages(msg) {
+
+    if(msg.type === "status") {
+        messages_On_Screen.innerHTML += 
+        `
+        <li class="${msg.type}">
+            <p><a> (${msg.time}) </a><strong> ${msg.from} </strong> ${msg.text} </p>
+        </li>
+        `
+    } else if(msg.type === "message") {
+        messages_On_Screen.innerHTML +=
+        `
+        <li class="${msg.type}">
+            <p><a> (${msg.time}) </a><strong> ${msg.from} </strong> para <strong> ${msg.to}: </strong> ${msg.text} </p>
+        </li>
+        `
+    }
+    document.querySelector(".fix").scrollIntoView({block: "end"});
+
+}
+
+setInterval(getMessages, 3000);
 
 function getUsers() {
     let promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
@@ -92,14 +127,24 @@ function getUsers() {
 }
 
 function seeUsers(answer) {
+    user_On_Screen.innerHTML=
+    `
+    <h1>Escolha um contato para enviar mensagem:</h1>
+    <div class="wrap" onclick="selectContact(this)">
+        <div>
+            <img src="assets/img/people 2.svg" alt="">
+            <h3>Todos</h3>
+        </div>
+        <img class="checked" src="assets/img/check.svg" alt="">
+    </div>
+    `;
     nomes = answer.data;
     addUserToList();
 }
 
 function addUserToList() {
-    let elemento = document.querySelector(".contact");
     for(let i = 0; i < nomes.length; i++){
-        elemento.innerHTML += 
+        user_On_Screen.innerHTML += 
         `            <div class="wrap" onclick="selectContact(this)">
                         <div>
                             <img src="assets/img/person-circle 1.svg" alt="">
@@ -109,4 +154,30 @@ function addUserToList() {
                     </div>`
     }
 }
+
+setInterval(getUsers, 10000);
+
+function sendMessage() {
+    my_message = document.querySelector("footer>input").value;
+    my_message_obj = 
+    {
+        from: nome,
+        to: "Todos",
+        text: my_message,
+        type: "message" // ou "private_message" para o bônus
+    }
+    let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', my_message_obj);
+    promise.then(messageSent);
+    promise.catch(errorMessage);
+}
+
+function messageSent() {
+    document.querySelector("footer>input").value = "";
+    getMessages();
+}
+
+function errorMessage() {
+    window.location.reload()
+}
+
 
